2c02e78673a4aba7ad2f874d7b1f1f9a370c7fa7
Calum Regan <c_regan1888@outlook.com>
2026-06-01 19:31:44 +0100

    dockerignore file

--- legal_clustering/__init__.py
+++ legal_clustering/__init__.py
@@ -2,4 +2,4 @@ from .document_clusterer import DocumentClusterer
 from .llm_evaluation import LLMEvaluation
 from .embedding_clusterer import EmbeddingClusterer
 from .full_evaluation import extract_contract_type, evaluate_clustering, print_comparison
-from legal_clustering import cluster_documents
+from .pipeline import cluster_documents
